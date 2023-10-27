1613ba8044df77d519b3d50d825dc0c7f38157af
Marius Grigoras <marius.grigoras@zaplabs.com>
2023-10-27 00:46:22 -0500

    enma-di initial setup

---
+++ src/main/java/com/enma/enmadi/EnmaDiApplication.java
@@ -0,0 +1,13 @@
+package com.enma.enmadi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnmaDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnmaDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enma/enmadi/EnmaDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enma.enmadi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnmaDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
