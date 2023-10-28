63cd9295570415c96fe0fd1abcc37e979ff4f958
Marius Grigoras <marius.grigoras@zaplabs.com>
2023-10-27 20:52:47 -0500

    di-assignment

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,15 +1,24 @@
 package guru.springframework.sfgdi.controllers;
 
+import org.springframework.stereotype.Controller;
+
 import guru.springframework.sfgdi.services.PetService;
 
 /**
  * Created by jt on 12/28/19.
  */
+@Controller
 public class PetController {
 
     private final PetService petService;
+    
+    public PetController(PetService petService) {
+		this.petService = petService;
+	}
 
-    public String whichPetIsTheBest(){
+
+
+	public String whichPetIsTheBest(){
         return petService.getPetType();
     }
 }
