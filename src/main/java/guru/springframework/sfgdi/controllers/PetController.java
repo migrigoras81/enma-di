9772fbd48d5a954c690a0118a4c4b94199472248
Marius Grigoras <marius.grigoras@zaplabs.com>
2023-10-31 11:38:23 -0500

    spring component scan

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -2,7 +2,7 @@ package guru.springframework.sfgdi.controllers;
 
 import org.springframework.stereotype.Controller;
 
-import guru.springframework.sfgdi.services.PetService;
+import com.springframework.pets.PetService;
 
 /**
  * Created by jt on 12/28/19.
