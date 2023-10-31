1c10f232efc487f0c658abbfc7d81e60a46d5ba1
Marius Grigoras <marius.grigoras@zaplabs.com>
2023-10-31 12:04:48 -0500

    spring java cofiguration primary profile beans

---
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package guru.springframework.sfgdi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import guru.springframework.sfgdi.services.I18NSpanishService;
+import guru.springframework.sfgdi.services.I18nEnglishGreetingService;
+import guru.springframework.sfgdi.services.PrimaryGreetingService;
+import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
+import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
+
+/**
+ * Created by Marius I. Grigoras
+ * 
+ * on 31/10/2023
+ */
+@Configuration
+public class GreetingServiceConfig {
+
+	@Primary
+	@Bean
+	PrimaryGreetingService primaryGreetingService() {
+		return new PrimaryGreetingService();
+	}
+	
+	@Bean
+	ConstructorGreetingService constructorGreetingService() {
+		return new ConstructorGreetingService();
+	}
+	
+	@Bean
+	PropertyInjectedGreetingService propertyInjectedGreetingService() {
+		return new PropertyInjectedGreetingService();
+	}
+	
+	@Bean
+	SetterInjectedGreetingService setterInjectedGreetingService() {
+		return new SetterInjectedGreetingService();
+	}
+	
+	@Profile("EN")
+	@Bean
+	I18nEnglishGreetingService i18nService() {
+		return new I18nEnglishGreetingService();
+	}
+	
+	@Profile({"ES","default"})
+	@Bean("i18nService")
+	I18NSpanishService i18nSpanishService() {
+		return new I18NSpanishService();
+	}
+}
--- src/main/java/guru/springframework/sfgdi/services/ConstructorGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/ConstructorGreetingService.java
@@ -5,10 +5,11 @@ import org.springframework.stereotype.Service;
 /**
  * Created by jt on 12/26/19.
  */
-@Service
+
 public class ConstructorGreetingService implements GreetingService {
-    @Override
-    public String sayGreeting() {
-        return "Hello World - Constructor";
-    }
+	
+	@Override
+	public String sayGreeting() {
+		return "Hello World - Constructor";
+	}
 }
